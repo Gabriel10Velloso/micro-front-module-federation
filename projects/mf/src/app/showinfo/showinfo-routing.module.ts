f2e51584370b1f47056ab34a707991fa3e9665e9
Gabriel10Velloso <gavelloso@gmail.com>
2022-04-10 18:31:28 -0300

    Esqueleto montado em forma de projetos antes do module-federation!

--- projects/mf/src/app/showinfo/showinfo-routing.module.ts
+++ projects/mf/src/app/showinfo/showinfo-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowinfoComponent } from './showinfo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShowinfoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
